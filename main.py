439cf091f9b0250fa4129d786c19ecf1036dab5b
glMuller <glmuller2001@gmail.com>
2025-11-13 23:15:13 -0300

    Create de colaborador feito.

--- main.py
+++ main.py
@@ -15,3 +15,33 @@ def abre_conexao():
     except Exception as e:
         print(f"Erro ao conectar ao banco: {e}")
         return None
+
+def adicionar_colaborador():
+    conn = abre_conexao()
+    if not conn:
+        print("Erro: Não foi possível conectar ao Banco de Dados.")
+        return
+
+    cursor = conn.cursor()
+
+    print("\n----- Cadastro de colaborador -----")
+    nome = input("Digite o nome do colaborador(a): ")
+    cpf = input("Digite o CPF do colaborador(a): ")
+    departamento = input("Digite o setor do colaborador(a)")
+
+    try:
+        sql = """
+            INSERT INTO COLABORADOR (NOME, DEPARTAMENTO, CPF)
+            VALUES (:1, :2, :3)
+        """
+
+        cursor.execute(sql, (nome, departamento, cpf))
+        conn.commit()
+
+        print("✅ Colaborador cadastrado com sucesso!")
+
+    except Exception as e:
+        print("Erro ao cadastrar:", e)
+
+    cursor.close()
+    conn.close()
