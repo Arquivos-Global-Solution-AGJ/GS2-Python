69919e758518fc90c52574201b2939f3c76af4dc
glMuller <glmuller2001@gmail.com>
2025-11-15 00:27:45 -0300

    Criada estrutura do menu.

--- main.py
+++ main.py
@@ -17,12 +17,7 @@ def abre_conexao():
         return None
 
 def adicionar_colaborador(conexao, cursor):
-    conn = abre_conexao()
-    if not conn:
-        print("Erro: Não foi possível conectar ao Banco de Dados.")
-        return
-
-    cursor = conn.cursor()
+    
 
     print("\n----- Cadastro de colaborador -----")
     nome = input("Digite o nome do colaborador(a): ")
@@ -36,24 +31,16 @@ def adicionar_colaborador(conexao, cursor):
         """
 
         cursor.execute(sql, (nome, departamento, cpf))
-        conn.commit()
+        conexao.commit()
 
         print("✅ Colaborador cadastrado com sucesso!")
 
     except Exception as e:
         print("Erro ao cadastrar:", e)
 
-    cursor.close()
-    conn.close()
+    
 
 def buscar_colaborador():
-    conn = abre_conexao()
-    if not conn:
-        print("Erro: Não foi possível conectar ao Banco de Dados.")
-        return
-
-    cursor = conn.cursor()
-
     try:
         cpf = input("Digite o CPF do colaborador: ").strip()
 
@@ -67,26 +54,60 @@ def buscar_colaborador():
         resultado = cursor.fetchone()
 
         if not resultado:
-            print("Nenhum paciente encontrado com esse CPF.")     
+            print("Nenhum colaborador encontrado com esse CPF.")     
             return
         try:
-            data_nascimento = resultado[2].strftime('%Y-%m-%d')
+            data_nascimento = resultado[4].strftime('%Y-%m-%d')
         except:
-            data_nascimento = str(resultado[2])
+            data_nascimento = str(resultado[4])
 
         print("\n----- Dados do colaborador -----")
         print(f"Matrícula colaborador: {resultado[0]}")
         print(f"Nome: {resultado[1]}")
         print(f"Departamento: {resultado[2]}")
         print(f"CPF: {resultado[3]}")
-        print(f"Data de nascimento: {resultado[4]}")
+        print(f"Data de nascimento: {data_nascimento}")
          
     except Exception as e:
         print(f"Erro ao consultar paciente: {e}") 
 
-    
-    query = '''
-        SELECT ID_COLABORADOR, NOME, DEPARTAMENTO, CPF
-        FROM COLABORADOR
-        ORDER BY ID_COLABORADOR
-    '''
+
+if __name__ == '__main__':
+    conexao = abre_conexao()
+    if not conexao:
+        print("Erro: Não foi possível conectar ao Banco de Dados.")
+        exit()
+    cursor = conexao.cursor()
+    em_execucao = True
+
+    while em_execucao:
+        print("\n----- Bem estar do seu colaborador -----")
+        print("0 - Encerrar programa")
+        print("1 - Inserir colaborador")
+        print("2 - Buscar colaborador por cpf")
+        print("3 - Quero registrar meus sentimentos")
+        print("4 - Excluir colaborador")
+        print("5 - Atualizar colaborador")
+        print("6 - Números de emergência")
+        try:
+            opcao = int(input("Escolha uma opção"))
+            print("-----")
+            match opcao:
+                case 0:
+                    em_execucao = False
+                case 1:
+                    adicionar_colaborador(conexao, cursor)
+                case 2:
+                    buscar_colaborador()
+                case 3:
+                    registrar_sentimento()
+                case 4:
+                    excluir_colaborador()
+                case 5:
+                    atualizar_colaborador()
+                case 6:
+                    numeros_emergencia()
+        except Exception as e:
+            print(f"Digite sua opção entre 0 e 6. {e}")
+    cursor.close()
+    conexao.close()
